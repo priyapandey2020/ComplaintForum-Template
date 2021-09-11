5c118702707deb874dab9f9215877f5b953a144e
JaiMadaan <77677308+JaiMadaan@users.noreply.github.com>
2021-09-11 17:20:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,12 @@ import firebase from 'firebase'
 require("@firebase/firestore")
 
 const firebaseConfig = {
- //Add configuration here
- 
+  apiKey: "AIzaSyBMqaRCtrKbwrBHrPH8lh3Ew9s4yeHXHBE",
+  authDomain: "complaintforum-86957.firebaseapp.com",
+  projectId: "complaintforum-86957",
+  storageBucket: "complaintforum-86957.appspot.com",
+  messagingSenderId: "487196378984",
+  appId: "1:487196378984:web:5a42c3fc8b5611879b38dd"
 };
   firebase.initializeApp(firebaseConfig);
 
