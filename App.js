13e078e9689b0e1f09611cdd20c05a8510fe96a5
sibani12345 <sanjanasawant1673@gmail.com>
2022-04-17 15:49:42 +0530

    change styling

--- App.js
+++ App.js
@@ -29,7 +29,7 @@ const TabNavigator = createBottomTabNavigator({
         return(
           <Image
           source={require("./assets/complaint.webp")}
-          style={{width:40, height:40}}
+          style={{width:50, height:50}}
         />
         )
         
